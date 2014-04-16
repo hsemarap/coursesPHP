a1c59648b8fae1a5f82d6a7885c3dc571eb37716
paramesh waran <hsemarap@gmail.com>
2014-04-16 20:10:17 +0530

    Departments page done

---
+++ dept.php
@@ -0,0 +1,125 @@
+<?php
+$dept=$_GET['query'];
+$dept=explode("/",$dept);
+$dept=$dept[1];
+require_once "connect.php";
+$query="SELECT * from departments where short = '$dept'";
+$res=mysql_query($query);
+if(mysql_num_rows($res)==0)$deptfound=false;
+else 
+while($row=mysql_fetch_assoc($res))
+{
+$deptfound=true;
+$deptid=$row['id'];
+$deptfull=$row['name'];
+$deptrollprefix=$row['rollno_prefix'];
+$depthodid=$row['hod_id'];
+}
+$query="SELECT * from faculties where id = '$depthodid'";
+$res=mysql_query($query);
+while($row=mysql_fetch_assoc($res))
+{
+$hodprefix=$row['prefix'].'a';
+$hoduid=$row['user_id'];
+$hodabout=$row['about'];
+}
+$query="SELECT * from users where id = '$hoduid'";
+$res=mysql_query($query);
+while($row=mysql_fetch_assoc($res))
+{
+$hodname=$row['name'];
+$hodemail=$row['email'];
+}
+?>
+<html>
+<head><title>Course Management System</title>
+<link rel='stylesheet' href='../css/bootstrap.min.css' />
+<link rel='stylesheet' href='../css/application_custom.css' />
+<script src='../js/jquery.min.js' ></script>
+<script src='../js/bootstrap.min.js' ></script>
+</head>
+<body >
+<style>
+.row{margin-right:0px!important;}
+.out{border:1px solid black;}
+.min100{min-height:100%;}
+.min90{min-height:90%;}.min80{min-height:80%;}.min70{min-height:70%;}
+.min60{min-height:60%;}.min50{min-height:50%;}.min40{min-height:40%;}
+.min30{min-height:30%;}.min20{min-height:20%;}.min10{min-height:10%;}
+</style>
+<div class='out container min80' style='width:100%;margin:auto'>
+  <div class='row min10'>
+    <div class='col1 min20 out'>
+    <img src='../images/nitt_logo.png' align='left' height='100' />
+    <h1 style='display:inline;position:relative;left:25%;bottom:-35px;'>Course Management System</h1>
+    <img src='../images/nitt_gj_logo.png' align='right' height='100' />    
+    </div>
+  </div>
+  <div class='row min10'>
+    <div class='col1 min80 out'>
+    <span style='position:relative;float:right' ><?php echo "$user->email ( ".$user->rank()." )";?></span>        
+	<div class="panel panel-default">
+	<a href='../' class='btn btn-info' >Go Back</a>
+	<br/>
+	<h1 style='display:inline'><?php echo $deptfull;?></h1><h2 style='display:inline'>(<?php echo $dept;?>)</h2>
+	<br/>Head of the Department : <h4 style='display:inline'><?php echo $hodprefix.$hodname;?></h4>
+    <table class="table table-hover">
+    <caption>
+      <h3>Courses Offered in <?php echo $deptfull;?></h3>
+    </caption>
+    <thead>
+      <tr>
+        <th>Course Name</th>
+        <th>Course Instructor</th>
+        <th>No. of credits</th>
+      </tr>
+    </thead>
+    <?php
+    	$query="SELECT * from terms where id in (SELECT term_id FROM term_departments WHERE department_id = '$deptid') order by semester";    	
+	$res=mysql_query($query);
+	while($row=mysql_fetch_assoc($res))
+	{
+		$ans[$row['semester']][]=$row;
+	}
+    ?>
+    <tbody>            
+    <?php
+    foreach($ans as $sem=>$semdetails){
+    echo "<tr><th colspan='3'>Sem #$sem</th></tr>";
+	    foreach($semdetails as $semester){
+	    	$courseid=$semester['course_id'];
+	        $termid=$semester['id'];
+                $query="SELECT * from courses where id = '$courseid'";    	
+		$res=mysql_query($query);
+//		echo $query;
+		while($row=mysql_fetch_assoc($res))
+		{
+			$subcode=$row['subject_code'];
+			$coursename=$row['name'];
+			$credits=$row['credits'];
+			$islab=$row['islaboratory'];
+		}
+		    echo "  <tr>
+			      <td>
+				<a href='/terms/$termid'>
+				  $subcode - $coursename
+				</a>
+			      </td>
+			      <td>
+				<small>
+				  
+				</small>
+			      </td>
+			      <td>$credits</td>
+			    </tr>";
+            }
+    }
+    ?>
+    </tbody>
+  </table>
+  </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
